08c353521eb30bbec9529de787ea1f402f6f7260
Yaqub24 <yaqub.eyvazli@gmail.com>
2023-04-29 20:25:18 +0400

    Book Shop
    Book Shop

---
+++ bookshoping/src/main/java/az/developia/bookshoping/BookshopingApplication.java
@@ -0,0 +1,13 @@
+package az.developia.bookshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopingApplication.class, args);
+	}
+
+}
---
+++ bookshoping/src/test/java/az/developia/bookshoping/BookshopingApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
